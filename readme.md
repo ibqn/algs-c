61f08e9ef37f79ae9c03fe4863f558f814f29204
Evgeny Bobkin <evgen.ibqn@gmail.com>
2019-07-05 22:37:54 +0200

    add josephus problem

--- readme.md
+++ readme.md
@@ -2,3 +2,9 @@
 
     gcc -O3 src/josephus-problem.c  -o josephus -Wall -pedantic -std=c11
     ./josephus 41 3
+
+## Buld using cmake
+
+    cmake -H. -Bbuild
+
+This will create a `build` directory and generate there all the required files.
--- src/josephus-problem.c
+++ src/josephus-problem.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <assert.h>
+
 
 typedef struct node_t node;
 
@@ -9,6 +11,22 @@ struct node_t {
     void *data;
     node *next;
 };
+
+node* node_new(void *data) {
+    node *tmp = malloc(sizeof(node));
+
+    tmp->data = data;
+    tmp->next = NULL;
+
+    return tmp;
+}
+
+typedef void (*data_free_fn)(void *);
+
+void node_destroy(node *n, data_free_fn data_free) {
+    data_free(n->data);
+    free(n);
+}
 
 typedef struct circular_list_t circular_list;
 
@@ -25,16 +43,80 @@ circular_list* circular_list_new(void) {
     return tmp;
 }
 
-circular_list_destroy(circular_list *list) {
+void circular_list_add(circular_list *list, void *data) {
+    assert(data != NULL);
 
+    node *tmp = node_new(data);
 
-    if (list) {
-        free(list);
+    if (list->tail) {
+        tmp->next = list->tail->next;
+        list->tail->next = tmp;
+        list->tail = tmp;
+    } else {
+        list->tail = tmp;
+        tmp->next = tmp;
     }
+
+    list->size++;
 }
 
-int main(int argc, char **argv)
-{
+node* circular_list_eliminate_every(circular_list *list, const int *num, data_free_fn data_free) {
+    assert(list != NULL);
+    assert(list->tail != NULL);
+
+    node *n = list->tail;
+
+    node* c;
+    while (n != n->next) {
+        for (int i = 1; i < *num; i++) {
+            n = n->next;
+        }
+        c = n->next;
+        printf("%d ", *((int *)c->data));
+        n->next = n->next->next;
+        node_destroy(c, data_free);
+    }
+    printf("done\n");
+
+    list->tail = n;
+
+    return n;
+}
+
+node* circular_list_get_first(circular_list *list) {
+    assert(list != NULL);
+    assert(list->tail != NULL);
+
+    return list->tail->next;
+}
+
+int circular_list_get_size(circular_list *list) {
+    assert(list != NULL);
+
+    return list->size;
+}
+
+void circular_list_destroy(circular_list *list, data_free_fn data_free) {
+    assert(list != NULL);
+
+    node *n = list->tail;
+
+    free(list);
+
+    if (n == NULL) {
+        return;
+    }
+
+    node *c;
+    node *tail = n;
+    do {
+        c = n;
+        n = n->next;
+        node_destroy(c, data_free);
+    } while (n != tail);
+}
+
+int main(int argc, char **argv) {
     int N = 41;
     int M = 3;
 
@@ -65,8 +147,34 @@ int main(int argc, char **argv)
 
     circular_list *list = circular_list_new();
 
+    int *data;
+    for(int i = 1; i <= N; i++) {
+        data = malloc(sizeof(int));
+        *data = i;
 
-    circular_list_destroy(list);
+        // printf("%d ", *data);
+
+        circular_list_add(list, (void *)data);
+    }
+    // printf("done\n");
+
+    int size = circular_list_get_size(list);
+    printf("Created circular list of size %d\n", size);
+
+    /* Cyrcle once */
+    node *first = circular_list_get_first(list);
+    node *n = first;
+    do {
+        printf("%d ", *((int *)n->data));
+        n = n->next;
+    } while (n != first);
+
+    printf("done\n");
+
+    node *last = circular_list_eliminate_every(list, &M, free);
+    printf("Last element is %d ", *((int *)last->data));
+
+    circular_list_destroy(list, free);
 
     return EXIT_SUCCESS;
 }
